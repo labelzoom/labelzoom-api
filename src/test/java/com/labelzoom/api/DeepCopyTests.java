729cc4ca0eab4b8ee242415cea1ad6280bb031f7
Rob Faust <robert.faust@labelzoom.net>
2023-12-25 11:57:59 -0600

    Updating tests

--- src/test/java/com/labelzoom/api/DeepCopyTests.java
+++ src/test/java/com/labelzoom/api/DeepCopyTests.java
@@ -1,9 +1,16 @@
 package com.labelzoom.api;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.labelzoom.api.model.components.AComponent;
 import com.labelzoom.api.model.components.CLabel;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
+import org.reflections.Reflections;
+import org.reflections.scanners.SubTypesScanner;
+import uk.co.jemos.podam.api.*;
+
+import java.lang.reflect.Modifier;
+import java.util.Set;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -28,5 +35,34 @@ public class DeepCopyTests
         final String labelSerialized = assertDoesNotThrow(() -> objectMapper.writeValueAsString(label));
         final String cloneSerialized = assertDoesNotThrow(() -> objectMapper.writeValueAsString(clone));
         assertEquals(labelSerialized, cloneSerialized);
+    }
+
+    @Test
+    @Disabled
+    void testMockClone()
+    {
+        final PodamFactory factory = new PodamFactoryImpl();
+        final CLabel label = factory.manufacturePojo(CLabel.class);
+        addRandomElements(label);
+        final CLabel clone = label.clone(true);
+        assertNotSame(label, clone);
+
+        final String labelSerialized = assertDoesNotThrow(() -> objectMapper.writeValueAsString(label));
+        final String cloneSerialized = assertDoesNotThrow(() -> objectMapper.writeValueAsString(clone));
+        System.out.println(labelSerialized);
+        System.out.println(cloneSerialized);
+        assertEquals(labelSerialized, cloneSerialized);
+    }
+
+    private static void addRandomElements(final CLabel label)
+    {
+        final PodamFactory factory = new PodamFactoryImpl();
+        final Reflections reflections = new Reflections("com.labelzoom.api.model.components", new SubTypesScanner());
+        final Set<Class<? extends AComponent>> allClasses = reflections.getSubTypesOf(AComponent.class);
+        for (final Class<?> clazz : allClasses) {
+            if (clazz.isInterface()) continue;
+            if (Modifier.isAbstract(clazz.getModifiers())) continue;
+            label.addElement((AComponent) factory.manufacturePojo(clazz));
+        }
     }
 }
