d824e904fac827ffe3412e43d05fef22bf5bf080
Robert Faust <robert.faust@labelzoom.net>
2026-01-20 16:40:15 -0600

    Renaming iterable `write` to `writeAll`

--- src/main/java/com/labelzoom/api/output/GenericWriter.java
+++ src/main/java/com/labelzoom/api/output/GenericWriter.java
@@ -3,7 +3,7 @@ package com.labelzoom.api.output;
 public interface GenericWriter<I> {
     void write(I item);
 
-    default void write(Iterable<I> items) {
+    default void writeAll(Iterable<I> items) {
         for (final I item : items) {
             write(item);
         }
